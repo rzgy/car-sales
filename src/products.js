3c6cb2b83bc90082fb465b434c88b1c7410eec96
Abdulrazak <rzookalq@gmail.com>
2024-05-12 20:25:01 +0300

    task 1 is done

---
+++ src/products.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    name: "Nissan Patrol",
+    price: 17000,
+    image:
+      "https://lh5.googleusercontent.com/wc0Z1sQWFSlMnQX7oMh-P8uj-VY0NId3O9G-0LHOBmpMEPIb9mwaa60Cj3CWSNgPsEFsAZDnuA3NfncmPvW8O5cFEPrn0cATEfcTx4pUbByp8PaATjuz3JlvnSMF3eE9eObAMBZmJDQAcNrQNEaKxsgNv9xW655th3gW6NejrGo8unAj_1a4-D_b3w",
+  },
+  {
+    name: "Toyota LandCrusier",
+    price: 19000,
+    image: "https://global.toyota/pages/news/images/2021/08/02/1330/001.jpg",
+  },
+  {
+    name: "Toyota Prado",
+    price: 12000,
+    image:
+      "https://s3-ap-southeast-2.amazonaws.com/imotor-cms/images_cms/b77faab1-a428-41fb-83cc-3eb52ad7428e.jpg",
+  },
+];
+
+export default products;
